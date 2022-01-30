314828e711eabbe9790b3a86c1a08f318f13c1bd
Ardo Kirsipuu <56802191+ardokirsipuu@users.noreply.github.com>
2022-01-31 00:30:30 +0200

    Wider Google Maps on the Contacts page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -16,32 +16,29 @@ const Contact = () => {
               <h1>{t("contact")}</h1>
             </div>
           </div>
-          <div className="row my-5">
-            <div className="col-sm-12 col-lg-6">
-              <h2>Arkinet OÜ</h2>
-              <ul className="unstyled">
-                <li>{t("reg_nr")}</li>
-                <li>{t("kmkr")}</li>
-                <li>{t("aadress")}</li>
-              </ul>
-              <h3>Andres Kirsipuu</h3>
-              <ul className="unstyled">
-                <li>(+372) 53 484 007</li>
-                <li>andres(at)arkinet.ee</li>
-              </ul>
-            </div>
-            <div className="col-sm-12 col-lg-4">
-              <iframe
-                title="Arkinet Oü asukoht kaardil"
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2116571.0507128644!2d25.068667!3d58.797813!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46ecc4fb3236c37f%3A0x9d0b7c57a6d83ca2!2zU3V1cmVqw7VlLCBQw6RybnUgQ291bnR5LCBFc3Rvbmlh!5e0!3m2!1sen!2sus!4v1642675965487!5m2!1sen!2sus"
-                width="100%"
-                height="100%"
-                style={{ border: 0 }}
-                loading="lazy"
-              ></iframe>
-            </div>
+          <div className="my-5">
+            <h2>Arkinet OÜ</h2>
+            <ul className="unstyled">
+              <li>{t("reg_nr")}</li>
+              <li>{t("kmkr")}</li>
+              <li>{t("aadress")}</li>
+            </ul>
+            <h3>Andres Kirsipuu</h3>
+            <ul className="unstyled">
+              <li>(+372) 53 484 007</li>
+              <li>andres(at)arkinet.ee</li>
+            </ul>
           </div>
         </div>
+        <iframe
+          title="Arkinet Oü asukoht kaardil"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3655957.23969596!2d22.29286203230245!3d58.87337470711787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46ecc4fa865e783b%3A0x289f9515f72492bf!2sArkinet%20O%C3%9C!5e0!3m2!1sen!2see!4v1643507815676!5m2!1sen!2see"
+          width="100%"
+          height="560"
+          style={{ border: 0 }}
+          allowfullscreen
+          loading="lazy"
+        ></iframe>
       </section>
     </Layout>
   )
