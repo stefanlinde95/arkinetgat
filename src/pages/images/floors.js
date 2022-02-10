6f4e9c8b4988acf6011290f46f5a0d19c4e6d856
Ardo Kirsipuu <56802191+ardokirsipuu@users.noreply.github.com>
2022-02-10 03:19:45 +0200

    Move reusable code of all the images pages into a Gallery component

--- src/pages/images/floors.js
+++ src/pages/images/floors.js
@@ -1,52 +1,12 @@
-import * as React from "react"
+import React from "react"
 import { graphql } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image"
-import Layout from "../../components/layout"
-import Seo from "../../components/seo"
-import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox"
 import { useTranslation } from "gatsby-plugin-react-i18next"
-import Galmenu from "../../components/galmenu"
+import Gallery from "../../components/gallery"
 
-function Floors({ data }) {
+const Floors = ({ data }) => {
   const { t } = useTranslation()
-  const images = data.allFile.edges
-  return (
-    <Layout>
-      <Seo title={t("floors")} />
-      <section>
-        <div className="container my-5">
-          <div className="mb-4">
-            <h1>{t("floors")}</h1>
-          </div>
-          <Galmenu />
-          <SimpleReactLightbox>
-            <SRLWrapper>
-              <div className="row">
-                {images.map(image => (
-                  <div
-                    className="img-col col-sm-12 col-md-6 col-lg-3"
-                    key={image.node.id}
-                  >
-                    <a
-                      href={image.node.publicURL}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <GatsbyImage
-                        className="ratio ratio-1x1"
-                        image={image.node.childImageSharp.gatsbyImageData}
-                        alt={`https://arkinetgatmaster.gatsbyjs.io${image.node.publicURL}`}
-                      />
-                    </a>
-                  </div>
-                ))}
-              </div>
-            </SRLWrapper>
-          </SimpleReactLightbox>
-        </div>
-      </section>
-    </Layout>
-  )
+
+  return <Gallery data={data} title={t("floors")} />
 }
 
 export default Floors
@@ -73,11 +33,12 @@ export const query = graphql`
       edges {
         node {
           childImageSharp {
-            gatsbyImageData(quality: 5)
+            gatsbyImageData(width: 320, quality: 50)
           }
           id
           publicURL
           base
+          relativePath
         }
       }
     }
